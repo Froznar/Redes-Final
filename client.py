4315c6e128513363d28f696a0ad610b18caeb323
Froznar <francisco.marin@ucsp.edu.pe>
2018-06-08 04:36:23 -0500

    servidor y cliente funcionando, servidor con threads

--- client.py
+++ client.py
@@ -1,10 +1,18 @@
-# Echo client program
+import socket
+import sys
+
+
 import socket
 
-HOST = '192.168.1.10'    # The remote host
-PORT = 50007              # The same port as used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+host = "192.168.1.3"
+port = 7777
+
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+soc.connect((host, port))
+
+clients_input = input("What you want to write client?\n")  
+soc.send(clients_input.encode("utf8")) # we must encode the string to bytes  
+result_bytes = soc.recv(4096) # the number means how the response can be in bytes  
+result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
+
+print("Result from server is {}".format(result_string))  
